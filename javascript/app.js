7ccf044de85f232e46a161829448cf725755af2d
louliakab <lolo888332@gmail.com>
2021-03-19 12:22:11 -0700

    creat js page

---
+++ javascript/app.js
@@ -0,0 +1,21 @@
+alert("are you getting bored with the same look"
+);
+
+alert("do you need a new style"
+);
+
+var answer= prompt(
+     "do you prefer get our service at home or at salon"
+ );
+  console.log(answer);
+  if(answer=='home'){
+    document.write('<img src="picture/home.jpg"/>');
+}else if (answer=='salon'){
+    document.write('<img src="picture/salon.jpg"/>');
+}else{
+    alert('choose betwen home or salon please ');
+}
+
+   $("select").change(function () {
+    var ID = $(this).children(":selected").attr("id");
+    $('#selectBox')
