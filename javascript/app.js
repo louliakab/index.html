d058ddbd3172e88e9423122cf637610b360bf39b
louliakab <lolo888332@gmail.com>
2021-03-21 14:39:03 -0700

    modify the home page

--- javascript/app.js
+++ javascript/app.js
@@ -4,14 +4,31 @@ alert("are you getting bored with the same look"
 alert("do you need a new style"
 );
 
-var answer= prompt(
-     "do you prefer get our service at home or at salon"
- );
-  console.log(answer);
-  if(answer=='home'){
-    document.write('<img src="picture/home.jpg"/>');
-}else if (answer=='salon'){
-    document.write('<img src="picture/salon.jpg"/>');
-}else{
+var answer = prompt(
+    "do you prefer get our service at home or at salon"
+);
+
+while (answer !== 'home' && answer !== 'salon') {
+    aswer = prompt('please only write home/salon');
+}
+var services = " "
+if (answer == 'home') {
+    services = '<img src="picture/home.jpg"/>';
+} else if (answer == 'salon') {
+    services = '<img src="picture/salon.jpg"/>';
+} else {
     alert('choose betwen home or salon please ');
 }
+
+var numberOfstar = prompt('What star rating would you give us');
+var stars = '';
+var result=""
+if (stars =>3) {
+    stars ='<img src="picture/star.jpg"/>';
+} else if (stars =>5) {
+    stars ='<img src="picture/star.jpg"/>';
+}
+for (var i = 0; i < numberOfstar; i++) {
+    result += stars;
+}
+
