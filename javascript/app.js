2471e03531125a6b292a7b2f00b36e9ae606fef8
louliakab <lolo888332@gmail.com>
2021-03-22 11:56:40 -0700

    add change

--- javascript/app.js
+++ javascript/app.js
@@ -3,11 +3,11 @@ alert("are you getting bored with the same look"
 
 alert("do you need a new style"
 );
-/*
+
 var answer = prompt(
     "do you prefer get our service at home or at salon"
 );
-/*
+
 while (answer !== 'home' && answer !== 'salon') {
     aswer = prompt('please only write home/salon');
 }
@@ -23,16 +23,9 @@ if (answer == 'home') {
 var numberOfstar = prompt ('What star rating would you give us');
 var stars = '';
 var result=""
-// if (stars ='3') {
-//     stars +='<img src="picture/star.jpg"/>';
-// }
-//  else if (stars ='5') {
-//     stars +='<img src="picture/star.jpg"/>';
-// }
-
 for (var i = 0; i < numberOfstar; i++) {
    stars +='<img src="picture/star.jpg"/>'
 }
 
 document.write(stars);
-© 2021 GitHub, 
+ 
