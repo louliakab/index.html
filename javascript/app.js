87c35537d081390cc3afd43d19db80b03b1a8dbe
louliakab <lolo888332@gmail.com>
2021-03-28 06:14:58 -0700

    new fill

--- javascript/app.js
+++ javascript/app.js
@@ -1,65 +1,65 @@
 
-alert("are you getting bored with the same look"
-);
+// alert("are you getting bored with the same look"
+// );
 
-alert("do you need a new style"
-); 
+// alert("do you need a new style"
+// ); 
 
-var answer = prompt(
-    "do you prefer get our service at home or at salon"
-);
+// var answer = prompt(
+//     "do you prefer get our service at home or at salon"
+// );
 
-while (answer !== 'home' && answer !== 'salon') {
-        aswer = prompt('please only write home/salon');
-    }
-    var services = ""
-    if (answer == 'home') {
-        services += '<img src="picture/home.jpg"/>';
-    } else if (answer == 'salon') {
-        services += '<img src="picture/salon.jpg"/>';
-    } else {
-        alert('choose betwen home or salon please ');
-    }
-    
-    var numberOfstar = prompt ('What star rating would you give us');
-    var stars = '';
-    var result=""
-    for (var i = 0; i < numberOfstar; i++) {
-       stars +='<img src="picture/star.jpg"/>'
-    }
-    document.write(stars);
-
-// var services= prompt("which services do you want to get?");
-// function getService(services);
-//  {
-
-//     if (services="nails" &&services="hair")  {
-//         alert('welcome to the salon');
-//     } else if
-//      (services="massage " || 'wax' )
-//      {
-//         alert('sorry we dont have this servies');
+// while (answer !== 'home' && answer !== 'salon') {
+//         aswer = prompt('please only write home/salon');
+//     }
+//     var services = ""
+//     if (answer == 'home') {
+//         services += '<img src="picture/home.jpg"/>';
+//     } else if (answer == 'salon') {
+//         services += '<img src="picture/salon.jpg"/>';
 //     } else {
-//         alert('please input the services you want');
+//         alert('choose betwen home or salon please ');
+//     }
+    
+//     var numberOfstar = prompt ('What star rating would you give us');
+//     var stars = '';
+//     var result=""
+//     for (var i = 0; i < numberOfstar; i++) {
+//        stars +='<img src="picture/star.jpg"/>'
+//     }
+//     document.write(stars);
+
+// // var services= prompt("which services do you want to get?");
+// // function getService(services);
+// //  {
+
+// //     if (services="nails" &&services="hair")  {
+// //         alert('welcome to the salon');
+// //     } else if
+// //      (services="massage " || 'wax' )
+// //      {
+// //         alert('sorry we dont have this servies');
+// //     } else {
+// //         alert('please input the services you want');
+// //     }
+// // }
+// // getservice(services);
+
+
+// var number = prompt("how many services you will take?");
+// function getNumber(services) {
+
+//     console.log(services);
+//     if (services >= 2 && services <= 5) {
+//         alert('welcome to the our salon');
+//     } else if (services < 0 || services > 0) {
+//         alert('sorry ');
+//     } else {
+//         alert('please input a number');
 //     }
 // }
-// getservice(services);
 
-
-var number = prompt("how many services you will take?");
-function getNumber(services) {
-
-    console.log(services);
-    if (services >= 2 && services <= 5) {
-        alert('welcome to the our salon');
-    } else if (services < 0 || services > 0) {
-        alert('sorry ');
-    } else {
-        alert('please input a number');
-    }
-}
-
-getnumber(servcies);
+// getnumber(servcies);
 
 
 
@@ -80,5 +80,4 @@ getnumber(servcies);
 
     //     result += services;
     // }
- 
-
+    for (var i = 4; i < 24; i = i + 1) {console.log(hello);}
